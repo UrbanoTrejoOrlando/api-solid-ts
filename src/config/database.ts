462030ea423f9aa754993c7480cb39114f338efd
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-02 10:36:11 -0600

    Modificacion al archivo database

---
+++ src/config/database.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const testConnection = async () => {
+  try {
+    await mongoose.connect(process.env.URL_MONGODB || "");
+    console.log("Conexión exitosa a MongoDB");
+    process.exit(0);
+  } catch (error) {
+    console.error("Error al conectar a MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+testConnection();
