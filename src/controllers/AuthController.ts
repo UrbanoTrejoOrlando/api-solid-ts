c6dd6cad861fc03c3549053dcd1be157e725628e
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-09 09:50:33 -0600

    Modificaciones a la carpeta controller

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { UserRepository } from "../repositories/UserRepository";
+import { AuthService } from "../services/AuthServices";
+
+const authService = new AuthService(new UserRepository());
+
+export class AuthController{
+    static async register(req:Request,res:Response){
+        try(
+            const token = await authService.register(req.body);
+        )catch(error){
+
+        }
+
+
+    }
+}
