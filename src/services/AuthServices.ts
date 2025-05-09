f1e856cecf4b35d9eb2f6df895a48c14e71d1d87
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-09 09:40:27 -0600

    Modificaciones a la carpeta services

--- src/services/AuthServices.ts
+++ src/services/AuthServices.ts
@@ -1,23 +1,29 @@
 import { LoginDTO, RegisterDTO } from "../dtos/UserDTO";
 import { UserRepository } from "../repositories/UserRepository";
 import { generateToken } from "../utils/jwt";
+import bcrypt from "bcrypt";
 
-export class AuthServices{
-    constructor(private userRepository: UserRepository){}
+export class AuthService {
 
-    // Creacion de funciones
-    async register(data: RegisterDTO){
+    constructor(private userRepository: UserRepository) { }
+
+
+
+    async register(data: RegisterDTO) {
         const exist = await this.userRepository.findByEmail(data.email);
         if (exist) throw new Error("User already exists");
         const user = await this.userRepository.create(data);
         return generateToken(user._id.toString());
 
     }
-
-    // Inicio de sesion
-    async login(data: LoginDTO){
+    async login(data: LoginDTO) {
         const user = await this.userRepository.findByEmail(data.email);
-        if(!user) throw new Error("User not found");
-        
+        if (!user) throw new Error("User not Found");
+        const isMach = bcrypt.compare(user.password, data.password);
+        if (!isMach) throw new Error("Invalid credentials");
+
+        return generateToken(user._id.toString());
+
     }
+
 }
