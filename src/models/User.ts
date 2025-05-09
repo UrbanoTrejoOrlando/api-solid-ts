afd929f5a9a8ca09426ec61efc280daff0ba113e
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-09 15:39:44 -0600

    Modificaciones a la carpeta models

--- src/models/User.ts
+++ src/models/User.ts
@@ -9,11 +9,11 @@ const UserSchema = new Schema ({
     },
     password:{
         type:String,
-        require:true,
+        required:true,
     },
 });
 
-UserSchema.pre("save", async (next)=>{
+UserSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password,salt);
@@ -22,3 +22,6 @@ UserSchema.pre("save", async (next)=>{
 });
 
 export const UserModel = model("User", UserSchema);
+
+
+;
