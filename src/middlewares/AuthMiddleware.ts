e62cf62cd54e77487af1230fdaa6bb371e9ed043
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-16 08:17:17 -0600

    Modificaciones

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -4,6 +4,14 @@ import { verifyToken } from "../utils/jwt";
 export const authMiddleware =(req:Request,res:Response, next:NextFunction)=>{
     const token = req.headers.authorization?.split(" ")[1];
     if(!token) return res.status(401).json({message: "Token no proporcionado"});
+    try {
+        const id = verifyToken(token);
+        req.body.user = id;
+        next();
+    } catch (error:any) {
+        res.status(401).json({message: error.message || "Token invalidado"})
+    }
+
 /**
  * headers:{
  *  "Content-Type": "application/json",
