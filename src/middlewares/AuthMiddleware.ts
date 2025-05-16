d80a1ac5a47e804d01147b2cdc42a50798871e9d
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-16 08:07:27 -0600

    Modificaciones

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -2,5 +2,12 @@ import { NextFunction, Request, Response } from "express";
 import { verifyToken } from "../utils/jwt";
 // Implementar un middleware para aunteticar 
 export const authMiddleware =(req:Request,res:Response, next:NextFunction)=>{
-    const token = req.headers.authorization?.split("")[1];
+    const token = req.headers.authorization?.split(" ")[1];
+/**
+ * headers:{
+ *  "Content-Type": "application/json",
+ *  "Authorization": "Bearer 123456"
+ * }
+ */
+
 }
