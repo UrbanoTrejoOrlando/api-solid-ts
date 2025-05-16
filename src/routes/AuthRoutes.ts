ff55c1cd0ba34150742739ac3e8748d69b9c094a
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-16 11:10:45 -0600

    Modificaciones

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
+router.get("/users", AuthController.getAllUsers);
 
 export default router; 
