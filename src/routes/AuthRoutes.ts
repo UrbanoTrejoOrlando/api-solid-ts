3f99b6b6084d0c656f1d6133def0cb751ee656e6
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-16 07:54:20 -0600

    Modificaciones

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -2,3 +2,6 @@ import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
 
 const router = Router();
+
+router.post("/register", AuthController.register);
+router.post("/login", AuthController.login);
