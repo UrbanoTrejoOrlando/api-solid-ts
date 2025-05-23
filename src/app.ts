4bc8c72d0482c30288d1396f56ef7884f95b8a07
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-23 08:56:45 -0600

    ·Modificaciones·

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 import authRoutes from "./routes/AuthRoutes";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", authRoutes);
